4a1f96a7e5dc78bb14470a9a74bbb4aaceaffb61
Andy <andy@mediasuite.co.nz>
2019-03-15 14:06:55 +1300

    Added server pagination tests

--- src/__tests__/components/Table.test.js
+++ src/__tests__/components/Table.test.js
@@ -234,4 +234,107 @@ describe('Table', () => {
         expect(lastName.sortable).toBe(false);
         expect(email.sortable).toBe(true);
     });
+
+    describe('when server pagination is used', () => {
+        let updateDataSpy;
+        let pagination = {};
+
+        beforeEach(() => {
+            updateDataSpy = jest.fn();
+            props = {
+                ...props,
+                updateData: updateDataSpy,
+            };
+            wrapper = mount(<Table { ...props } />);
+            instance = wrapper.instance();
+        });
+
+        describe('nextPage()', () => {
+            it('should call updateData with correct page when currentPage < totalPages', () => {
+                pagination = {
+                    currentPage: 1,
+                    totalPages: 2,
+                }
+                wrapper.setState({ pagination });
+                instance.nextPage();
+
+                expect(updateDataSpy).toHaveBeenCalledTimes(1);
+                expect(updateDataSpy).toHaveBeenCalledWith({page: pagination.currentPage + 1});
+            });
+
+            it('should not call updateData if currentPage >= totalPages', () => {
+                pagination = {
+                    totalPages: 2,
+                    currentPage: 2,
+                }
+                wrapper.setState({ pagination });
+                instance.nextPage();
+
+                expect(updateDataSpy).not.toHaveBeenCalled();
+            });
+        });
+
+        describe('previousPage()', () => {
+            it('should call updateData with correct page when currentPage > 1', () => {
+                pagination = {
+                    currentPage: 2,
+                    totalPages: 4,
+                }
+                wrapper.setState({ pagination });
+                instance.previousPage();
+
+                expect(updateDataSpy).toHaveBeenCalledTimes(1);
+                expect(updateDataSpy).toHaveBeenCalledWith({page: pagination.currentPage - 1});
+            });
+
+            it('should not call updateData if currentPage <= 1', () => {
+                pagination = {
+                    totalPages: 1,
+                    currentPage: 1,
+                }
+                wrapper.setState({ pagination });
+                instance.previousPage();
+
+                expect(updateDataSpy).not.toHaveBeenCalled();
+            });
+        });
+    });
+
+    describe('displayedRows', () => {
+        it('should equal row length when server pagination is used', () => {
+            jest.spyOn(tableActions, 'calculateRows').mockReturnValue([]);
+            props = {
+                ...props,
+                rows: [
+                    { firstName: 'Paul', lastName: 'Darragh', isOpen: true }
+                ],
+                pagination: {
+                    isServerPagination: true,
+                },
+            }
+            wrapper = mount(<Table { ...props } />);
+            instance = wrapper.instance();
+
+            expect(wrapper.state().rows.length).toBe(props.rows.length);
+        });
+
+        it('should equal calculateRows result when sever pagination is not used', () => {
+            const rows = [
+                { firstName: 'Paul', lastName: 'Darragh', isOpen: true },
+                { firstName: 'Dave', lastName: 'Smith', isOpen: true },
+            ];
+            jest.spyOn(tableActions, 'calculateRows').mockReturnValue(rows);
+            props = {
+                ...props,
+                rows,
+                pagination: {
+                    isServerPagination: false,
+                },
+            }
+            wrapper = mount(<Table { ...props } />);
+            instance = wrapper.instance();
+
+            expect(wrapper.state().rows.length).toBe(rows.length);
+        });
+    });
 });
